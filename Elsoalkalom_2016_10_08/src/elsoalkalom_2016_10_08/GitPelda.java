5598e9d62b65fdd2ea78b769b87257c8ce88b6d4
tanulo <tanulo@192.168.225.25>
2017-06-03 13:34:07 +0200

    GitPélda az első alkalomhoz.

---
+++ Elsoalkalom_2016_10_08/src/elsoalkalom_2016_10_08/GitPelda.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package elsoalkalom_2016_10_08;
+
+/**
+ *
+ * @author tanulo
+ */
+public class GitPelda {
+
+}
